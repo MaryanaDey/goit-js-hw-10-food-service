afb674ae272c641c7e92628b70d4166cd09a2669
MaryanaDey <81640002+MaryanaDey@users.noreply.github.com>
2021-10-09 19:13:49 +0300

    fixid home work

--- src/js/checkbox.js
+++ src/js/checkbox.js
@@ -1,50 +1,40 @@
 const refs = {
-    checkbox: document.querySelector('#theme-switch-toggle'),
-    body: document.querySelector('body'),
-  };
-  const Theme = {
-    LIGHT: 'light-theme',
-    DARK: 'dark-theme',
-  };
-  
-  currentCheckThem();
-  checkboxPosition();
-  
-  refs.checkbox.addEventListener('change', onBlackWhite);
-  
-  function onBlackWhite(e) {
-    const checkThem = localStorage.getItem('Them');
-    if (e.target.checked) {
-      localStorage.setItem('Them', Theme.DARK);
-      refs.body.classList.add(Theme.DARK);
-    } else {
-      localStorage.setItem('Them', Theme.LIGHT);
-      refs.body.classList.remove(Theme.DARK);
-    }
+  checkbox: document.querySelector('#theme-switch-toggle'),
+  body: document.querySelector('body'),
+};
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const checkTheme = localStorage.getItem('Theme');
+
+
+checkboxPosition();
+
+refs.checkbox.addEventListener('change', onClickCheck);
+
+function onClickCheck(evt) {
+
+  if (evt.target.checked) {
+    localStorage.setItem('Theme', Theme.DARK);
+    refs.body.classList.add(Theme.DARK);
+   
+  } else {
+    localStorage.setItem('Theme', Theme.LIGHT);
+    refs.body.classList.remove(Theme.DARK);
+    
+    refs.body.classList.add(Theme.LIGHT);
   }
-  
-  function checkboxPosition() {
-    const checkThem = localStorage.getItem('Them');
-    if (checkThem === Theme.DARK) {
-      refs.checkbox.checked = true;
-    }
+}
+
+function checkboxPosition() {
+
+  if (checkTheme === Theme.DARK) {
+    refs.checkbox.checked = true;
   }
-  
-  function currentCheckThem() {
-    const checkThem = localStorage.getItem('Them');
-  
-    if (!checkThem) {
-      localStorage.setItem('Them', Theme.LIGHT);
-      refs.body.classList.add(Theme.LIGHT);
-    } else {
-      refs.body.classList.add(checkThem);
-    }
-  }
-
-
-
-
-
+}
 
 
 
